efa79fc77946299648364b6f60d2032330f20958
EC2 Default User <ec2-user@ip-172-31-7-5.us-west-1.compute.internal>
2022-03-03 21:55:29 +0000

    add ports for renewal

--- certbot/renew.sh
+++ certbot/renew.sh
@@ -3,8 +3,16 @@ SECONDS="$(awk 'BEGIN{srand(); print int(rand()*(3600+1))}')"
 echo "Sleeping for $SECONDS seconds"
 sleep $SECONDS
 
+echo "stopping nginx"
+docker stop nginx_proxy
+
+echo "renewing certs"
 docker run -it --rm --name certbot \
         -v "/etc/letsencrypt:/etc/letsencrypt" \
         -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-        certbot/certbot renew -q
+	-p 80:80 \
+	-p 443:443 \
+        certbot/certbot renew
 
+echo "starting nginx again"
+docker start nginx_proxy
