52245b900b86a651e9b7062e501ef4f54005e951
Capp Wiedenhoefer <33261505+cappww@users.noreply.github.com>
2022-11-05 12:25:11 -0700

    Update manage.sh

--- certbot/manage.sh
+++ certbot/manage.sh
@@ -1,4 +1,4 @@
-sudo docker run -it --rm --name certbot \
-	-v "/etc/letsencrypt:/etc/letsencrypt" \
-	-v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-  certbot/certbot certificates
+docker run -it --rm --name certbot \
+        -v "/etc/letsencrypt:/etc/letsencrypt" \
+        -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+        certbot/certbot certificates
